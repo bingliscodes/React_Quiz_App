6326b4de61c414f123daa5967ed44791a03a5581
Benjamin Inglis <bjammin134@gmail.com>
2025-05-21 08:42:12 -0700

    Add questions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/quiz-logo.png";
 export default function Header() {
   return (
     <header>
-      <img src={logo} />
+      <img src={logo} alt="Quiz logo" />
       <h1>ReactQuiz</h1>
     </header>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,1 +1,9 @@
-export default function Quiz() {}
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  return <p>{QUESTIONS[activeQuestionIndex].text}</p>;
+}
