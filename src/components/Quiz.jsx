b2dade2d879a21c33fd765a7843ed5ff7d1dc670
Benjamin Inglis <bjammin134@gmail.com>
2025-05-21 08:53:26 -0700

    Display quiz answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,5 +5,22 @@ export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
-  return <p>{QUESTIONS[activeQuestionIndex].text}</p>;
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((ans, idx) => (
+            <li key={ans} className="answer">
+              <button onClick={() => handleSelectAnswer(ans)}>{ans}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
